0af08549d8b0ea62d6803050c7d367d319554d75
Thangkyro <79921592+Thangkyro@users.noreply.github.com>
2024-05-25 11:19:45 +0700

    Add code

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        string getText(int to)
+        {
+            string resultf = "";
+            if (to%3==0 && to%5==0)
+            {
+                resultf = "StarRez";
+            }
+            else if (to%3==0)
+            {
+                resultf = "Star";
+            }
+            else if (to%5==0)
+            {
+                resultf = "Rez";
+            }
+            else
+            {
+                resultf = to.ToString();
+            }
+            return resultf;
+        }
+
+        [HttpGet(Name = "GetAll")]
+        public IEnumerable<TestClass> Get(int from, int to)
+        {
+            return Enumerable.Range(from, to - from + 1).Select(index => new TestClass
+            {
+                kidNumber = index,
+                kidResponse = getText(index)
+            })
+            .ToArray();
+        }
+
+        [HttpPost()]
+        public ResultfClass Collect(TestClass testClass)
+        {
+            ResultfClass resultfClass = new ResultfClass();
+            resultfClass.isValid = (testClass.kidResponse == getText(testClass.kidNumber));
+            resultfClass.expectedResponse = getText(testClass.kidNumber);
+            return resultfClass;
+        }
+    }
+}
---
+++ WebApplication1/TestClass.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1
+{
+    public class TestClass
+    {
+        public int kidNumber { get; set; }
+        public string kidResponse { get; set; }
+    }
+
+    public class ResultfClass
+    {
+        public bool isValid { get; set; }
+        public string expectedResponse { get; set; }
+
+        public string error { get; set; }
+    }
+}
